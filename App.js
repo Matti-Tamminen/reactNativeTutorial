aafa4d16ea60c6a079a6e3e2e170dd9039eb79df
MattiT <matti.tamminen@codecontrol.fi>
2024-02-27 16:02:58 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hei hei</Text>
+      <Text>This is kinda cool</Text>
+      <StatusBar style='auto' />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
